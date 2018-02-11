44ae1b2c4810ef6214fc46a1f70fb90ee241f822
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-11 14:28:37 +0100

    project completed

--- components/characterList.js
+++ components/characterList.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addCharacterById } from '../actions';
 
-class CharacterList extends Component {
-    //state = {}
-    render() {
-        return (
-            <div>
-                <h4 className="text-uppercase">Characters</h4>
-                <ul className="list-group">
-                    {
-                        this.props.characters.map(item => {
-                            return (
-                                <li key={item.id} className="list-group-item">
-                                    <div className="list-item">{item.id}.-{item.name}</div>
-                                    <div onClick={() => this.props.addCharacterById(item.id)} className="list-item right-button"> + </div>
-                                </li>
-                            );
-                        })
-                    }
-                </ul>
-            </div>
-        );
-    }
+const CharacterList = ({ characters, addCharacterById }) => {
+    return (
+        <div>
+            <h4 className="text-uppercase">Characters</h4>
+            <ul className="list-group">
+                {
+                    characters.map(item => {
+                        return (
+                            <li key={item.id} className="list-group-item">
+                                <div className="list-item">{item.id}.-{item.name}</div>
+                                <div onClick={() => addCharacterById(item.id)} className="list-item right-button"> + </div>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 function mapStateToProps(state) {
